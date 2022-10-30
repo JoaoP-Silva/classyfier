a4e7bd2ca04e8aac32486fb3cf1d3be5f9d371a3
Joao Pedro <joopedrof.silva@gmail.com>
2022-10-29 22:03:58 -0300

    Refazendo a função Closest.
    Na implementação anterior, foi utilizado uma modificação do algoritmo descrito na seção 22.4 do livro Algorithm Desing and Applications, Goodrich e Tamisa, porém nos testes finais do programa foi identificado que a modificação deste algoritmo não estava calculando o resultado correto. Sendo assim a função ClosestPoints() foi refeita utilizando uma árvore kd. De acordo com a seção 21.3 do mesmo livro citado anteriormente, uma consulta na árvore kd tem custo no caso médio de o(log(n)), sendo assim, a função ClosestPoints executa em tempo O(n log (n)), com n sendo o tamanho da envoltória com mais pontos que foi passada como parâmetro.

--- scripts/Closest.py
+++ scripts/Closest.py
@@ -1,80 +1,53 @@
-import copy
 import numpy as np
 from convexHull import Node
+from scipy import spatial
 
-def euclidiana(p1,p2):
-  return np.sqrt(((p1.x-p2.x)**2 )+((p1.y-p2.y)**2))
-
-def ForcaBruta(N, tam):
-  min_val = float('inf')
-  closest_1 = Node(10,0,"A")
-  closest_2 = Node(10,0,"B")
-  for i in range(tam):
-    for j in range(i+1,tam):
-      dist = euclidiana(N[i],N[j])
-      if (dist < min_val and N[i].label != N[j].label):
-        min_val = dist
-        closest_1 = N[i]
-        closest_2 = N[j]
-  
-  return min_val, closest_1,closest_2
-
-def ClosestStrip(strip,tam,d):
-  min_val = d
-  closest_1 = Node(10,0,"A")
-  closest_2 = Node(10,0,"B")
-  for i in range(tam):
-    j = i + 1
-    while j< tam and ((strip[j].y - strip[i].y) < min_val):
-      if(strip[j].label != strip[i].label):
-        min_val = euclidiana(strip[i], strip[j])
-        closest_1 = strip[i]
-        closest_2 = strip[j]
-      j += 1
-  
-  return min_val, closest_1,closest_2
-
-def Closest(N, N_copy, tam):
-  if tam <= 3:
-    return ForcaBruta(N,tam)
-  
-  meio = tam//2
-  PontoMedio = N[meio]
-
-  N_l = N[:meio]
-  N_r = N[meio:]
-
-  d_l, aux, aux2 = Closest(N_l, N_copy, meio)
-  d_r, aux3, aux4 = Closest(N_r, N_copy, tam - meio)
-
-  d = min(d_l,d_r)
-
-  stripN = []
-  stripN_c = []
-  lr = N_l + N_r
-  for i in range(tam):
-    if abs(lr[i].x - PontoMedio.x) < d:
-      stripN.append(lr[i])
-    if abs(N_copy[i].x - PontoMedio.x) < d:
-      stripN_c.append(N_copy[i])
-  
-  stripN.sort(key = lambda point: point.y)
-  d_N, close1,close2 = ClosestStrip(stripN, len(stripN), d)
-  d_N_copy, close3,close4 = ClosestStrip(stripN_c, len(stripN_c), d)
-                          
-
-  min_a = min(d, d_N) 
-  min_b = min(d, d_N_copy)
-  if(min_a < min_b):
-    return min_a, close1,close2
-  else:
-    return min_b, close3,close4
-
+#Using kd-trees, realizes querys to find the closest points from two
+#convexH ulls.
 def ClosestPoints(chA, chB):
-  N = chA + chB
-  N.sort(key = lambda point: point.x)
-  N_copy = copy.deepcopy(N)
-  N_copy.sort(key = lambda point: point.y)
-  result = Closest(N, N_copy, len(N))
+  chA_x = np.array([])
+  chA_y = np.array([])
+  
+  #np array to consult closest points A in B
+  arrayChA = np.empty((0,2), float)
 
-  return result[1], result[2]
+  #Building A kd-tree
+  for point in chA:
+      chA_x = np.append(chA_x, point.x)
+      chA_y = np.append(chA_y, point.y)
+      arrayChA = np.append(arrayChA, np.array([[point.x, point.y]]), axis = 0)
+  kdTreeA = spatial.KDTree(np.c_[chA_x.ravel(), chA_y.ravel()])
+
+      
+  chB_x = np.array([])
+  chB_y = np.array([])
+
+  #np array to consult closest points B in A
+  arrayChB = np.empty((0,2), float)
+
+  #Building B kd-tree
+  for point in chB:
+      chB_x = np.append(chB_x, point.x)
+      chB_y = np.append(chB_y, point.y)
+      arrayChB = np.append(arrayChB, np.array([[point.x, point.y]]), axis = 0)
+  kdTreeB = spatial.KDTree(np.c_[chB_x.ravel(), chB_y.ravel()])
+
+  #For n points in A calculates the n closest points in B
+  dist_closestOfB, i_closestOfB = kdTreeB.query(arrayChA)
+
+  #For n points in B calculates the n closest points in A 
+  dist_closestOfA, i_closestOfA= kdTreeA.query(arrayChB)
+
+  result = "null"
+  #For each pair of closest points between the hulls, returns the smaller
+  for i in range(len(i_closestOfB)):
+      B_index = i_closestOfB.item(i)
+      if(i_closestOfA.item(B_index) == i):
+          if result == "null":
+              result = (dist_closestOfB.item(i), i, B_index)
+          elif dist_closestOfA.item(B_index) < result[0] :
+              result = (dist_closestOfB.item(i), i, B_index)
+  
+  indexA = result[1]
+  indexB = result[2]
+  return (chA[indexA], chB[indexB])
